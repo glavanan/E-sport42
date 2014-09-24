c24255b16dc24ddd715310c58d065b8ecfd48f6d
Guillian LAVANANT <glavanan@agau.(none)>
2014-09-24 16:17:19 +0200

    Impossible de valider formulaire

--- tuto_django/Member/views.py
+++ tuto_django/Member/views.py
@@ -11,10 +11,22 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 
-class MemberForm(ModelForm):
-	class Meta:
-		model = Member
 
+class AccountForm(forms.Form):
+	def __init__(self, *args, **kwargs):
+		request=kwargs.pop("first_name")
+		pays = kwargs.pop("pays")
+		super(AccountForm, self).__init__(*args, **kwargs)
+		self.fields['first_name'] = forms.CharField(label = 'Nom', required=True, max_length=30, initial=request)
+#	last_name = forms.CharField(label = 'Prenom', required=True, max_length=30, value=request.user.last_name)
+#	email = forms.EmailField(required=True, value=str(request.user.email))
+#		self.fields['age'] = forms.IntegerField(initial=age, required=False)
+	      	self.fields['pays'] = forms.CharField(max_length=30, initial=pays, required=False)
+   #     ville = forms.CharField(max_length=60, value=request.user.ville)
+    #    adresse = forms.TextField(value=request.user.adresse)
+     #   pseudo_lol = forms.CharField(max_length=30, value=request.user.pseudo_lol)
+      #  duoquadra = forms.CharField(max_length=10, value=request.user.duoquadra)
+	
 class UserForm(forms.Form):
 	username = forms.CharField(required=True, max_length=30)
 	first_name = forms.CharField(label = 'Nom', required=True, max_length=30)
@@ -23,7 +35,7 @@ class UserForm(forms.Form):
 	email = forms.EmailField(required=True)
 
 def home(request):
-	return render_to_response('Member/home.html')
+	return render_to_response('Member/home.html', {'user' : request.user.username})
 
 def register(request):
 	text = ""
@@ -64,13 +76,32 @@ def login(request):
 		else:
 			return render_to_response('Member/login.html', {'login_form' : login_form, 'message' : text}, context_instance=RequestContext(request))
 	else:
-		return render_to_response('Member/home.html')
+		return render_to_response('Member/home.html', {'user' : request.user.username})
 
 def logout(request):
 	auth_logout(request)
 	login_form = LoginForm()
 	text = ""
 	return render_to_response('Member/login.html', {'login_form' : login_form, 'message' : text}, context_instance=RequestContext(request))
+
+def my_account(request):
+	text = ""
+	member = Member(user=request.user)
+	account_form=AccountForm(first_name = request.user.first_name, pays = request.user.member.pays)
+	if request.method == 'POST':
+		if account_form.is_valid():
+			text="VALIDE"
+			user.first_name, user.age = account_form.cleaned_data['first_name'], account_form.cleaned_data['pays']
+		else:
+			print account_form.errors
+			text=account_form.errors
+			return render_to_response('Member/account.html', {'account_form' : account_form, 'message' : text}, context_instance=RequestContext(request))
+	else:
+		if request.user.is_authenticated():
+			return render_to_response('Member/account.html', {'account_form' : account_form, 'message' : text}, context_instance=RequestContext(request))
+		else:
+			return render_to_response('Member/home.html', {'user' : request.user.username})
+
 
 def success(request):
 	return HttpResponse('<p>Success</p>')
