17d0336801b9a14a749158fd6955b270397a1ad0
OddNaughty <camwag.w@gmail.com>
2015-03-03 00:04:09 +0100

    Fixed url bug and install drf_nested_routers

--- esport42/esport42/urls.py
+++ esport42/esport42/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from rest_framework import routers
+from rest_framework_nested import routers
 from base.views import MyUserViewSet
 
+
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'accounts', MyUserViewSet)
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'esport42.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^api/v1/', include(router.urls)),
-	url(r'', include('base.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+					   url(r'^api/v1/', include(router.urls)),
+					   # url(r'', include('base.urls')),
+					   url(r'^admin/', include(admin.site.urls)),
 )
