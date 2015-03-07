01cb11b24690a2e2f680031eacee916efca14c93
Guillian LAVANANT <glavanan@e3r12p5.42.fr>
2015-03-07 17:40:34 +0100

    full add

--- esport42/esport42/urls.py
+++ esport42/esport42/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework_nested import routers
 from base.views import MyUserViewSet, IndexView
+from post.views import PostViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'accounts', MyUserViewSet)
+router.register(r'post', PostViewSet)
 
 urlpatterns = patterns('',
 					url(r'^api/v1/', include(router.urls)),
