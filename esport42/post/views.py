bb2a97b33c383fe9fbde824b0ecb4410b4b527a7
OdNaughty <cwagner@student.42.fr>
2015-03-16 19:56:56 +0100

    Testing some shitty shit again

--- esport42/post/views.py
+++ esport42/post/views.py
@@ -1,27 +1,16 @@
-from django.shortcuts import render
-from rest_framework import permissions, viewsets, status
-from rest_framework.response import Response
+from rest_framework import permissions, viewsets
+from base.permissions import IsOwnerOrAdmin
 from post.models import Post
-from post.permissions import IsAdminOfSite
 from post.serializers import PostSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = (IsAdminOfSite,)
-
 
     def perform_create(self, serializer):
-        print self.request.user
-        if self.request.user.get('is_admin', None):
-            instance = serializer.save(author=self.request.user)
-            return super(PostViewSet, self).perform_create(serializer)
-        return Response({'status' : 'Bad request',
-                         'message' : 'post could not be created with received data.'}, status=status.HTTP_400_BAD_REQUEST)
+        instance = serializer.save(author=self.request.user)
+        return super(PostViewSet, self).perform_create(serializer)
 
-
-class AdminPostViewSet(viewsets.ViewSet):
-    queryset = Post.objects.order_by('-created_at')
-    serializer_class = PostSerializer
-
-# Create your views here.
+    def get_permissions(self):
+        return ((permissions.AllowAny(),) if self.request.method == 'POST'
+                else (permissions.IsAdminUser(), IsOwnerOrAdmin()))
