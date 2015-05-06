6195be061947b544a87ada9e5c58d428d38f2be2
OddNaughty <camwag.w@gmail.com>
2015-05-06 14:16:43 +0200

    gitignore is working good and mofdified settings

--- esport42/esport42/settings.py
+++ esport42/esport42/settings.py
@@ -41,8 +41,7 @@ INSTALLED_APPS = (
     'post',
     'tournoi',
     'djangular',
-    'rest_framework',
-    'jsonify',
+    'rest_framework'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -65,8 +64,12 @@ WSGI_APPLICATION = 'esport42.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'esport42',
+	'USER': 'esport42',
+	'PASSWORD': os.getenv("DB_PASS"),
+	'HOST': 'localhost',
+	'PORT': ''
     }
 }
 
